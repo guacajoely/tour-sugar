ea22c497fa5c1b3d5e3e8b9e76f569caf52ca61f
guacajoely <guacajoely@gmail.com>
2023-03-06 11:48:06 -0500

    band and venue alerts working. fixed date strings

--- scripts/database.js
+++ scripts/database.js
@@ -2,33 +2,38 @@ const database = {
 
     bands: [
         { 
+        id: 1,
         name: "The New Doodz", 
         memberCount: 3,
         genre: "country",
         founded: 2022
         },
         { 
-            name: "Hoopity Hooplahs", 
-            memberCount: 4,
-            genre: "punk",
-            founded: 1999
+        id: 2,
+        name: "Hoopity Hooplahs", 
+        memberCount: 4,
+        genre: "punk",
+        founded: 1999
         },
         { 
-            name: "Jimbos 1manband", 
-            memberCount: 1,
-            genre: "folk",
-            founded: 2010
+        id: 3,
+        name: "Jimbos 1manband", 
+        memberCount: 1,
+        genre: "folk",
+        founded: 2010
         }
     ],
 
     venues: [
         { 
+        id: 1,
         name: "Pittstainz",
         address: "100 Main Street",
         sqFeet: 3000,
         maxOcupancy: 250
         },
         { 
+        id: 2,
         name: "That One Place",
         address: "5000 3rd Avenue",
         sqFeet: 3700,
@@ -38,13 +43,30 @@ const database = {
 
     bookings: [
         {
+        id: 1,
+        bandID: 2,
+        venueID: 1,
         bandName: "Hoopity Hooplahs",
         venueName: "Pittstainz",
-        bookDate: 3
+        bookDate: 1612638112396
+        },
+        {
+        id: 2,
+        bandID: 1,
+        venueID: 2,
+        bandName: "The New Doodz",
+        venueName: "That One Place",
+        bookDate: 1614528112396
+        },
+        {
+        id: 3,
+        bandID: 2,
+        venueID: 2,
+        bandName: "Hoopity Hooplahs",
+        venueName: "That One Place",
+        bookDate: 1614598112396
         }
- ]
-
- 
+    ]
 }
 
 
